05cdaa3fe7e83d8656532d25745764e65c714dc8
Julianus Pfeuffer <8102638+jpfeuffer@users.noreply.github.com>
2025-07-05 12:40:08 +0200

    Fix bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,8 @@ nmake /f Makefile.win clean all
 if errorlevel 1 exit 1
 
 REM Install step
-copy windows\libsvm.dll %LIBRARY_LIB%\libsvm.dll
+copy windows\libsvm.lib %LIBRARY_LIB%\libsvm.lib
+copy windows\libsvm.dll %LIBRARY_BIN%\libsvm.dll
 if errorlevel 1 exit 1
 xcopy windows\*.exe %LIBRARY_BIN%\
 if errorlevel 1 exit 1
